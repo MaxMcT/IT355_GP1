c89b18c6bca09faed3374fef99ed0cb7adc685b6
1m36 <1m36jm@gmail.com>
2024-10-11 19:29:02 -0500

    added createTempFile method

--- src/main/java/bank/SafeSQL.java
+++ src/main/java/bank/SafeSQL.java
@@ -75,7 +75,7 @@ public class SafeSQL {
 
         return preparedStatement.executeUpdate();
     }
-
+    
     static int openAccount(String name) throws SQLException {
         Connection dbConnection = dbConnect();
 
@@ -90,6 +90,32 @@ public class SafeSQL {
         return preparedStatement.executeUpdate();
     }
 
+     /**
+     * Temporary file is created and deleted before termination
+     */
+    static void createTempFile() {
+        File tempFile = null;
+        try {
+            tempFile = File.createTempFile("temp", ".txt");
+            System.out.println("Temporary file created: " + tempFile.getAbsolutePath());
+        } 
+        catch (IOException e) {
+            System.out.println("Error creating file: " + e);
+        } 
+        finally {
+            if (tempFile != null && tempFile.exists()) {
+                //Temp file no longer in use and is deleted
+                boolean deleted = tempFile.delete();
+                if (deleted) {
+                    System.out.println("Temporary file deleted: " + tempFile.getAbsolutePath());
+                } 
+                else {
+                    System.err.println("Failed to delete temporary file: " + tempFile.getAbsolutePath());
+                }
+            }
+        }
+    }
+    
     public static void main(String[] args) throws SQLException {
         System.out.println(deposit(new BigDecimal(500.001), "Billy"));
     }
