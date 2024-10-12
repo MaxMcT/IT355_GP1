cf4633066d395d388cecb1219607ba0032e3cca6
1m36 <1m36jm@gmail.com>
2024-10-11 19:46:04 -0500

    createTempFile

--- src/main/java/bank/Bank.java
+++ src/main/java/bank/Bank.java
@@ -1,5 +1,31 @@
 public class Bank {
 
+    /**
+     * Temporary file is created and deleted before termination
+     */
+    public void createTempFileGood() {
+        File tempFile = null;
+        try {
+            tempFile = File.createTempFile("temp", ".txt");
+            System.out.println("Temporary file created: " + tempFile.getAbsolutePath());
+        } 
+        catch (IOException e) {
+            System.out.println("Error creating file: " + e);
+        } 
+        finally {
+            if (tempFile != null && tempFile.exists()) {
+                //Temp file no longer in use and is deleted
+                boolean deleted = tempFile.delete();
+                if (deleted) {
+                    System.out.println("Temporary file deleted: " + tempFile.getAbsolutePath());
+                } 
+                else {
+                    System.err.println("Failed to delete temporary file: " + tempFile.getAbsolutePath());
+                }
+            }
+        }
+    }
+  
   public static void main(String[] args) {
 
   }
