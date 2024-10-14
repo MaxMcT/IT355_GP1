fc2fbd1783bcb3b3b3f40f20c75bb219cb4a15fe
Trolzer <39714370+Trolzer@users.noreply.github.com>
2024-10-13 19:28:40 -0500

    minor change so compiler stops yelling over an example

--- src/main/java/rule_examples/MET50.java
+++ src/main/java/rule_examples/MET50.java
@@ -8,14 +8,17 @@ public class MET50 {
 
     public static MET50 constructA(int i, String b){
         //constructA code
+        return null;
     }
 
     public static MET50 constructB(String b, int i){
         //constructB code
+        return null;
     }
 
     public static MET50 constructC(double j, int i, String b){
         //constructB code
+        return null;
     }
 
 }
