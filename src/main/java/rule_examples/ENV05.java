b86d651728cf9d842e3a1f510d70ac55ce7f2da4
Zach Contreras <158154908+Zach729@users.noreply.github.com>
2024-10-13 22:55:36 -0500

    Create ENV05.java

---
+++ src/main/java/rule_examples/ENV05.java
@@ -0,0 +1,16 @@
+package rule_examples;
+
+/**
+ * ENV05-J: Do not deploy an application that can be remotely monitored
+ * 
+ * Author: Zach Contreras
+ *
+ * This class ensures that remote monitoring is disabled for security.
+ */
+public class RemoteMonitoringExample {
+    public static void disableRemoteMonitoring() {
+        // Code to disable remote monitoring features
+        System.setProperty("com.sun.management.jmxremote", "false");
+        System.setProperty("java.security.debug", "none");
+    }
+}
